04401a0591f55a453a7e5b7c62039456def09b52
Darren Foley <6538994+darrenfoley@users.noreply.github.com>
2023-11-21 18:13:35 -0700

    Add say command for bot owner

---
+++ src/Asiago/Commands/OwnerModule.cs
@@ -0,0 +1,28 @@
+﻿using DSharpPlus;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+using DSharpPlus.Entities;
+
+namespace Asiago.Commands
+{
+    internal class OwnerModule : BaseCommandModule
+    {
+        // Dependency-injected members
+        public DiscordClient Discord { private get; set; } = null!;
+
+        [Command("say")]
+        [RequireOwner]
+        public async Task Say(CommandContext _, ulong channelId, [RemainingText] string message)
+        {
+            var channel = await Discord.GetChannelAsync(channelId);
+
+            DiscordEmbedBuilder embedBuilder = new()
+            {
+                Color = Colours.EmbedColour,
+                Description = message,
+            };
+
+            await channel.SendMessageAsync(embedBuilder);
+        }
+    }
+}
--- src/Asiago/Program.cs
+++ src/Asiago/Program.cs
@@ -1,7 +1,9 @@
 ﻿using Asiago;
+using Asiago.Commands;
 using Asiago.Exceptions;
 using Asiago.SlashCommands;
 using DSharpPlus;
+using DSharpPlus.CommandsNext;
 using DSharpPlus.SlashCommands;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -12,16 +14,13 @@ var builder = new ConfigurationBuilder()
 
 var config = builder.Build();
 
-var serviceCollection = new ServiceCollection();
-
-serviceCollection.AddOptions<IsThereAnyDealOptions>().Bind(config.GetRequiredSection("IsThereAnyDeal")).ValidateDataAnnotations();
-serviceCollection.AddSingleton<HttpClient>();
-
-var serviceProvider = serviceCollection.BuildServiceProvider();
-
 if (config.GetRequiredSection("Discord").GetValue<string>("Token") is not string token)
 {
-    throw new ConfigurationException("Discord token must be set to a string in config.json");
+    throw new ConfigurationException("Discord.Token must be set to a string in config.json");
+}
+if (config.GetRequiredSection("Discord").GetValue<string>("CommandPrefix") is not string commandPrefix)
+{
+    throw new ConfigurationException("Discord.CommandPrefix must be set to a string in config.json");
 }
 
 DiscordConfiguration discordConfig = new()
@@ -29,10 +28,17 @@ DiscordConfiguration discordConfig = new()
     Token = token,
     TokenType = TokenType.Bot,
     // TODO: narrow down needed intents
-    Intents = DiscordIntents.AllUnprivileged | DiscordIntents.MessageContents
+    Intents = DiscordIntents.AllUnprivileged
 };
-
 DiscordClient discord = new(discordConfig);
+
+var serviceCollection = new ServiceCollection();
+
+serviceCollection.AddOptions<IsThereAnyDealOptions>().Bind(config.GetRequiredSection("IsThereAnyDeal")).ValidateDataAnnotations();
+serviceCollection.AddSingleton<HttpClient>()
+    .AddSingleton(discord);
+
+var serviceProvider = serviceCollection.BuildServiceProvider();
 
 var slashCommands = discord.UseSlashCommands(new SlashCommandsConfiguration
 {
@@ -41,6 +47,15 @@ var slashCommands = discord.UseSlashCommands(new SlashCommandsConfiguration
 
 // TODO: conditionally register commands for guild/globally based on config.json
 slashCommands.RegisterCommands<IsThereAnyDealModule>();
+
+// Remember: need to add DiscordIntents.MessageContents privileged intent if prefix commands need to work outside of DMs with the bot
+var commands = discord.UseCommandsNext(new CommandsNextConfiguration
+{
+    Services = serviceProvider,
+    StringPrefixes = new[] { commandPrefix },
+});
+
+commands.RegisterCommands<OwnerModule>();
 
 await discord.ConnectAsync();
 await Task.Delay(-1);
