2d0e60b0a7b74cdcc85f5aa0b80b8f4e189d679d
Darren Foley <6538994+darrenfoley@users.noreply.github.com>
2023-12-14 03:28:30 -0700

    Add postgres db support via ef core
    -Added an initial model and migration

---
+++ src/Asiago.Data/Extensions/IConfigurationRootExtensions.cs
@@ -0,0 +1,54 @@
+﻿using Asiago.Common.Exceptions;
+using Microsoft.Extensions.Configuration;
+using Npgsql;
+
+namespace Asiago.Data.Extensions
+{
+    public static class IConfigurationRootExtensions
+    {
+        public static string? GetPostgresConnectionString(this IConfigurationRoot config)
+        {
+            string? connectionString = null;
+
+            try
+            {
+                NpgsqlConnectionStringBuilder connectionStringBuilder;
+
+                // Prioritize full connection string over pieces
+                if (config.GetValue<string>("POSTGRES_CONNECTION_STRING") is string dbConnectionString)
+                {
+                    connectionStringBuilder = new(dbConnectionString);
+                }
+                else
+                {
+                    connectionStringBuilder = new()
+                    {
+                        Host = config.GetValue<string>("POSTGRES_HOST"),
+                        Port = config.GetValue<int>("POSTGRES_PORT"),
+                        Database = config.GetValue<string>("POSTGRES_DB") ?? "asiago",
+                        Username = config.GetValue<string>("POSTGRES_USER"),
+                        Password = config.GetValue<string>("POSTGRES_PASSWORD"),
+                        Pooling = true,
+                    };
+                }
+
+                connectionString = connectionStringBuilder.ConnectionString;
+            }
+            catch (ArgumentException) { }
+
+            return connectionString;
+        }
+
+        public static string GetRequiredPostgresConnectionString(this IConfigurationRoot config)
+        {
+            string? connectionString = config.GetPostgresConnectionString();
+            if (string.IsNullOrWhiteSpace(connectionString))
+            {
+                throw new ConfigurationException(
+                    "Unable to construct postgres connection string. Missing/invalid environment POSTGRES_* environment variables?"
+                    );
+            }
+            return connectionString;
+        }
+    }
+}
--- src/Asiago/Program.cs
+++ src/Asiago/Program.cs
@@ -1,10 +1,13 @@
 ﻿using Asiago;
 using Asiago.Commands;
+using Asiago.Data;
+using Asiago.Data.Extensions;
 using Asiago.Extensions;
 using Asiago.SlashCommands;
 using DSharpPlus;
 using DSharpPlus.CommandsNext;
 using DSharpPlus.SlashCommands;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -16,6 +19,7 @@ var config = builder.Build();
 string token = config.GetRequiredValue<string>("DISCORD_TOKEN");
 string commandPrefix = config.GetRequiredValue<string>("DISCORD_COMMANDPREFIX");
 string isThereAnyDealApiKey = config.GetRequiredValue<string>("ISTHEREANYDEAL_APIKEY");
+string postgresConnectionString = config.GetRequiredPostgresConnectionString();
 
 DiscordConfiguration discordConfig = new()
 {
@@ -29,7 +33,8 @@ DiscordClient discord = new(discordConfig);
 var serviceCollection = new ServiceCollection();
 
 serviceCollection.AddOptions<IsThereAnyDealOptions>().Configure(options => options.ApiKey = isThereAnyDealApiKey);
-serviceCollection.AddSingleton<HttpClient>();
+serviceCollection.AddSingleton<HttpClient>()
+    .AddDbContext<ApplicationDbContext>(options => options.UseNpgsql(postgresConnectionString));
 
 var serviceProvider = serviceCollection.BuildServiceProvider();
 
