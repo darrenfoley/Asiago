0b8c3ea7209657e680ccb204a131108de4290866
Darren Foley <6538994+darrenfoley@users.noreply.github.com>
2024-04-15 22:41:34 -0600

    Send Twitch notifications on Discord as a nice looking embed

---
+++ src/Asiago/Extensions/DiscordEmbedBuilderExtensions.cs
@@ -0,0 +1,16 @@
+﻿using DSharpPlus.Entities;
+
+namespace Asiago.Extensions
+{
+    /// <summary>
+    /// Extension methods on <see cref="DiscordEmbedBuilder"/>.
+    /// </summary>
+    internal static class DiscordEmbedBuilderExtensions
+    {
+        /// <summary>
+        /// Sets the embed's footer for Asiago Notifications.
+        /// </summary>
+        public static DiscordEmbedBuilder WithAsiagoNotificationFooter(this DiscordEmbedBuilder embedBuilder)
+            => embedBuilder.WithFooter("Asiago Notifications").WithTimestamp(DateTimeOffset.Now);
+    }
+}
